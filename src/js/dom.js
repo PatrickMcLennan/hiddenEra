03d5828b97f9f28fdf239a7420b4f6af3543d9d9
Patrick <patrick.a.mclennan@gmail.com>
2018-12-03 00:53:46 -0500

    sass

---
+++ src/js/dom.js
@@ -0,0 +1,18 @@
+dom = {
+  socialIcon: {
+    facebook: document.querySelector('#facebook'),
+    instagram: document.querySelector('#instagram'),
+    youtube: document.querySelector('#youtube'),
+    snapchat: document.querySelector('#snapchat'),
+  },
+
+  home: {
+    section: document.querySelector('.home'),
+    header: document.querySelector('.home__header'),
+    reviewsRow: document.querySelector('.home__reviews'),
+    reviewsLeft: document.querySelector('.home__reviewsLeft'),
+    reviewsRight: document.querySelector('.home__reviewsRight'),
+  }
+}
+
+export default dom;
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import "../sass/main.scss";
+import dom from './dom.js';
+
+
+const hello = () => dom.home.header.innerText = 'hello?'
+
+hello();
+
